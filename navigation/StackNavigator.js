e8cd7348191b7a18eb4187682edb8aa3f2a0b750
Thomas Garza <tbgarza2@gmail.com>
2020-03-21 12:53:07 -0500

    added chat and show listing screens

--- navigation/StackNavigator.js
+++ navigation/StackNavigator.js
@@ -1,24 +1,27 @@
 import * as React from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
 
-// import BottomTabNavigator from './navigation/BottomTabNavigator';
 import DrawerNavigator from './DrawerNavigator';
+import TabBarIcon from '../components/TabBarIcon'
 
 import LogInScreen from '../screens/LogInScreen';
+import ChatScreen from '../screens/ChatScreen';
+import ShowListingScreen from '../screens/ShowListingScreen';
 
 const Stack = createStackNavigator();
 
-const StackNavigator = () => {
-  const [auth, setAuth] = React.useState(null);
-  const changeAuth = () => setAuth(auth = true);
-  console.log(auth);
-  return (
+const StackNavigator = (navigation) => (
     <Stack.Navigator headerMode='none'>
       <Stack.Screen name="Login" component={LogInScreen} />
-      <Stack.Screen name="Drawer" component={DrawerNavigator} />
-      {/* <Stack.Screen name="Root" component={BottomTabNavigator} /> */}
+      <Stack.Screen name="Drawer" component={DrawerNavigator} 
+        options={{ headerTitle: null, 
+        headerLeft: () => <TabBarIcon 
+        name="md-menu" 
+        onPress={() => navigation.toggleDrawer()} />}}/>
+        <Stack.Screen name="ChatScreen" component={ChatScreen} />
+        <Stack.Screen name="ShowListing" component={ShowListingScreen} />
     </Stack.Navigator>
   )
-}
+
 
 export default StackNavigator;

b9cd05c80a25651efc4703c46f313bb2c0039bdf
Thomas Garza <tbgarza2@gmail.com>
2020-03-21 09:20:21 -0500

    added stack for tabs in drawer

--- navigation/DrawerNavigator.js
+++ navigation/DrawerNavigator.js
@@ -8,19 +8,39 @@ import ProfileScreen from '../screens/ProfileScreen';
 import CreateListingScreen from '../screens/CreateListingScreen';
 import MessagesScreen from '../screens/MessagesScreen';
 import LogInScreen from '../screens/LogInScreen'
+import BottomTabNavigator from './BottomTabNavigator';
+import { createStackNavigator } from '@react-navigation/stack';
+import TabBarIcon from '../components/TabBarIcon';
 
 const Drawer = createDrawerNavigator();
-
-const DrawerNavigator = () => {
-    return (
+const DrawerStack  = createStackNavigator()
+//    { BottomTabNavigator: BottomTabNavigator
+//   },
+//   {
+//     defaultNavigationOptions: ({ navigation }) => {
+//       return {
+//         headerLeft: (
+        //   <TabBarIcon name="md-menu" onPress={() => navigation.openDrawer()} />
+//         )
+//       };
+//     }
+//   }
+// );
+const DrawerStackNavigator = () => (
+    <DrawerStack.Navigator>
+        <DrawerStack.Screen name="Tabs" component={BottomTabNavigator} options={{headerTitle: null, headerLeft: ({navigation}) => <TabBarIcon name="md-menu" onPress={() => navigation.toggleDrawer()} />}} />
+    </DrawerStack.Navigator>
+)
+const DrawerNavigator = () =>  (
         <Drawer.Navigator>
             <Drawer.Screen name="Home" component={HomeScreen} />
             <Drawer.Screen name="Profile" component={ProfileScreen} />
             <Drawer.Screen name="Create Listing" component={CreateListingScreen} />
             <Drawer.Screen name="Messages" component={MessagesScreen} />
             <Drawer.Screen name="Log Out" component={LogInScreen} />
+            <Drawer.Screen name="Tabs" component={DrawerStackNavigator} />
         </Drawer.Navigator>
     )
-}
+
 
 export default DrawerNavigator;

00bdad100702b0434a0b84b7fa0108ab5041c619
Thomas Garza <tbgarza2@gmail.com>
2020-03-21 15:23:01 -0500

    added menu button and headers to drawer

--- navigation/StackNavigator.js
+++ navigation/StackNavigator.js
@@ -1,9 +1,7 @@
 import * as React from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
-
 import DrawerNavigator from './DrawerNavigator';
 import TabBarIcon from '../components/TabBarIcon'
-
 import LogInScreen from '../screens/LogInScreen';
 import ChatScreen from '../screens/ChatScreen';
 import ShowListingScreen from '../screens/ShowListingScreen';
@@ -11,12 +9,14 @@ import ShowListingScreen from '../screens/ShowListingScreen';
 const Stack = createStackNavigator();
 
 const StackNavigator = (navigation) => (
-    <Stack.Navigator headerMode='none'>
+    <Stack.Navigator>
       <Stack.Screen name="Login" component={LogInScreen} />
       <Stack.Screen name="Drawer" component={DrawerNavigator} 
         options={{ headerTitle: null, 
         headerLeft: () => <TabBarIcon 
         name="md-menu" 
+        size={30}
+        style={{ marginBottom: -3 }}
         onPress={() => navigation.toggleDrawer()} />}}/>
         <Stack.Screen name="ChatScreen" component={ChatScreen} />
         <Stack.Screen name="ShowListing" component={ShowListingScreen} />

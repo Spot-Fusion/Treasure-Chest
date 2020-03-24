c265c7c15c87c5c36128407ba985f9dd65085469
Thomas Garza <tbgarza2@gmail.com>
2020-03-23 22:00:47 -0500

    created expo image picker

---
+++ components/ExpoImagePicker.js
@@ -0,0 +1,52 @@
+import * as React from 'react';
+import { Button, Image, View } from 'react-native';
+import * as ImagePicker from 'expo-image-picker';
+import Constants from 'expo-constants';
+import * as Permissions from 'expo-permissions';
+
+const ExpoImagePicker = () => {
+  const [image, setImage] = React.useState(null);
+
+const  getPermissionAsync = async () => {
+    if (Constants.platform.ios) {
+      const { status } = await Permissions.askAsync(Permissions.CAMERA_ROLL);
+      if (status !== 'granted') {
+        alert('Sorry, we need camera roll permissions to make this work!');
+      }
+    }
+  }
+
+  React.useEffect(() => {
+    getPermissionAsync();
+    console.log('hi');
+}, [])
+
+const _pickImage = async () => {
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.All,
+      allowsEditing: true,
+      aspect: [4, 3],
+      quality: 1
+    });
+
+    console.log(result);
+
+    if (!result.cancelled) {
+      setImage(result.uri);
+    }
+  };
+
+    return (
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        {image &&
+          <Image source={{ uri: image }} style={{ width: 100, height: 100, borderRadius: 50 }} />}
+          <Button
+            title="Pick an image from camera roll"
+            onPress={_pickImage}
+          />
+      </View>
+    );
+  
+}
+
+export default ExpoImagePicker;

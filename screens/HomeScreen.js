a28eef54ff20f14c23cc80ba53344e679800443e
JJohnson45 <Jd.johnson452@gmail.com>
2020-03-24 10:34:46 -0500

    fixedHeaderProb

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -5,56 +5,63 @@ import { NavigationContainer } from '@react-navigation/native';
 import TabBarIcon from '../components/TabBarIcon'
 import BottomTabNavigator from '../navigation/BottomTabNavigator';
 import Expo from 'expo';
-import { Ionicons } from '@expo/vector-icons';
+//import { Ionicons } from '@expo/vector-icons';
+import {Container, Content, Header, Left, Right, Icon} from 'native-base';
+import { render } from 'react-dom';
 
-
-const DATA = [
-  { key: <Ionicons name="md-image" size={100} style={{ marginBottom: -3 }}/>, id: 1 },
-  { key: <Ionicons name="md-image" size={100} style={{ marginBottom: -3 }}/>, id: 2 },
-  { key: <Ionicons name="md-image" size={100} style={{ marginBottom: -3 }}/>, id: 3 },
-  { key: <Ionicons name="md-image" size={100} style={{ marginBottom: -3 }}/>, id: 4 },
-  { key: <Ionicons name="md-image" size={100} style={{ marginBottom: -3 }}/>, id: 5 },
-  { key: <Ionicons name="md-image" size={100} style={{ marginBottom: -3 }}/>, id: 6 },
-  { key: <Ionicons name="md-image" size={100} style={{ marginBottom: -3 }}/>, id: 7 },
-  { key: <Ionicons name="md-image" size={100} style={{ marginBottom: -3 }}/>, id: 8 },
-  { key: <Ionicons name="md-image" size={100} style={{ marginBottom: -3 }}/>, id: 9 },
-  { key: <Ionicons name="md-image" size={100} style={{ marginBottom: -3 }}/>, id: 10 },
-  ];
+// const DATA = [
+//   { key: <Ionicons name="md-image" size={100} style={{ marginBottom: -3 }}/>, id: 1 },
+//   { key: <Ionicons name="md-image" size={100} style={{ marginBottom: -3 }}/>, id: 2 },
+//   { key: <Ionicons name="md-image" size={100} style={{ marginBottom: -3 }}/>, id: 3 },
+//   { key: <Ionicons name="md-image" size={100} style={{ marginBottom: -3 }}/>, id: 4 },
+//   { key: <Ionicons name="md-image" size={100} style={{ marginBottom: -3 }}/>, id: 5 },
+//   { key: <Ionicons name="md-image" size={100} style={{ marginBottom: -3 }}/>, id: 6 },
+//   { key: <Ionicons name="md-image" size={100} style={{ marginBottom: -3 }}/>, id: 7 },
+//   { key: <Ionicons name="md-image" size={100} style={{ marginBottom: -3 }}/>, id: 8 },
+//   { key: <Ionicons name="md-image" size={100} style={{ marginBottom: -3 }}/>, id: 9 },
+//   { key: <Ionicons name="md-image" size={100} style={{ marginBottom: -3 }}/>, id: 10 },
+//   ];
 
 const HomeScreen = ({ route, navigation}) => {
   console.log(route.params);
+  
   return (
-    <View style={styles.container}>
-      <Button title= 'Menu' onPress={() => navigation.toggleDrawer()} />
-        <Button title="Go to Create Listings" onPress={() => navigation.navigate('Create Listing')} />
-        <View style={styles.container}>
-    <ScrollView scrollEventThrottle= {16}>
-          <View style={{ flex: 1, backgroundColor: 'white', paddingHorizontal: 20}}>
-              <Text style = {{fontSize: 24, fontWeight: '700', paddingHorizontal: 20}} > Wassup shopper</Text>
-              <View style = {{height:130, marginTop: 20}}>
-                <ScrollView>
-                  <View style = {{height: 130, height:130}}>
-                    <View style = {{flex:2}}>
+    <Container>
+      <Header style={{ backgroundColor: '#3a455c', height: 90, borderBottomColor: '#757575'}}>
+        
+      </Header>
+    </Container>
+    // <View style={styles.container}>
+    //   <Button title= 'Menu' onPress={() => navigation.toggleDrawer()} />
+    //     <Button title="Go to Create Listings" onPress={() => navigation.navigate('Create Listing')} />
+    //     <View style={styles.container}>
+    // <ScrollView scrollEventThrottle= {16}>
+    //       <View style={{ flex: 1, backgroundColor: 'white', paddingHorizontal: 20}}>
+    //           <Text style = {{fontSize: 24, fontWeight: '700', paddingHorizontal: 20}} > Wassup shopper</Text>
+    //           <View style = {{height:130, marginTop: 20}}>
+    //             <ScrollView>
+    //               <View style = {{height: 130, height:130}}>
+    //                 <View style = {{flex:2}}>
                     
-                    </View>
-                    <View style = {{flex:1}}>
+    //                 </View>
+    //                 <View style = {{flex:1}}>
 
-                    </View>
-                  </View>
-                </ScrollView>
-              </View>
-          </View>
-    </ScrollView>
-        {/* <FlatList
-          data={DATA}
-          renderItem={({ item }) => <TouchableOpacity style={{justifyContent: 'center'}}>{item.key}</TouchableOpacity>}
-          keyExtractor={item => item.id.toString()}
-        /> */}
-      </View>
-    </View>
+    //                 </View>
+    //               </View>
+    //             </ScrollView>
+    //           </View>
+    //       </View>
+    // </ScrollView>
+    //     <FlatList
+    //       data={DATA}
+    //       renderItem={({ item }) => <TouchableOpacity style={{justifyContent: 'center'}}>{item.key}</TouchableOpacity>}
+    //       keyExtractor={item => item.id.toString()}
+    //     /> 
+    //   </View>
+    // </View>
   );
-}
 
+}
 HomeScreen.navigationOptions = {
   header: null,
 };

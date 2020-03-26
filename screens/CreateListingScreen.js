11bcc75cf9ca8e22ba8dcbd2fa7bae60d52f977e
Thomas Garza <tbgarza2@gmail.com>
2020-03-25 20:17:06 -0500

    added dynamic url for team to work on local host or emulator

--- screens/CreateListingScreen.js
+++ screens/CreateListingScreen.js
@@ -19,8 +19,10 @@ import axios from 'axios';
    const [negotiable, setNegotialbe] = React.useState(0);   
    const [idListing, setIdListing] = React.useState(0);
 
+   let url = 'localhost' || '10.0.2.2';
+
    const addPost = async (name, description, price, zipcode, negotiable) => {
-    await axios.post(`http://localhost:8080/listing/`, {id_seller: idSeller, id_category: 1, name, description, price, zipcode, negotiable })
+    await axios.post(`http://${url}:8080/listing/`, {id_seller: idSeller, id_category: 1, name, description, price, zipcode, negotiable })
     .then(id => setIdListing(id.data))
     .catch(e => console.error(e));
   }

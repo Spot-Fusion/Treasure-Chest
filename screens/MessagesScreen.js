d2646fc252eb5d5a22505dbe2d7f518c2bf3cb7f
Thomas Garza <tbgarza2@gmail.com>
2020-03-27 19:26:45 -0500

    fixed url and removed the interval for gettting messages

--- screens/MessagesScreen.js
+++ screens/MessagesScreen.js
@@ -9,12 +9,12 @@ const MessagesScreen = ({ navigation }) => {
   const [users, setUsers] = React.useState([]);
 
   const getUsers = async (id) => {
-    let users = await axios.get(`http://'10.0.2.2':8080/message/users/${id}`)
+    let users = await axios.get(`http://10.0.2.2:8080/message/users/${id}`)
     setUsers(users.data);
   }
 
   const getId = async (email) => {
-    let user = await axios.get(`http://'10.0.2.2':8080/user/${email}`)
+    let user = await axios.get(`http://10.0.2.2:8080/user/${email}`)
     console.log('id:',user.data.id);
     return user.data.id
   }
@@ -26,8 +26,8 @@ const MessagesScreen = ({ navigation }) => {
   let idUser = global.id || 1
   React.useEffect(() => {
     getUsers(idUser)
-    let interval = setInterval(() => { getUsers(idUser) }, 1000);
-    return () => clearInterval(interval);
+    // let interval = setInterval(() => { getUsers(idUser) }, 1000);
+    // return () => clearInterval(interval);
   }, []);
 
   return (

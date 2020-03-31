f441204e18be60ac0e28720285043ece308d81ea
Thomas Garza <tbgarza2@gmail.com>
2020-03-31 15:12:29 -0500

    added dynamic url

--- navigation/DrawerNavigator.js
+++ navigation/DrawerNavigator.js
@@ -28,34 +28,36 @@ const DrawerStackNavigator = ({ navigation }) => (
 const DrawerContent = (props, {user}) => {
  
   return (
-  <SafeAreaView style={{flex: 1}}>    
-    <DrawerItem name="top" label={() => (
-      <View style={{ flex: 1, flexDirection: "row" }}>
-        <Image
-          style={{ height: 70, width: 70, borderRadius: 35, resizeMode: "contain" }}
-          source={{
-            uri: !user ? "http://pngimg.com/uploads/tiger/tiger_PNG23245.png" : user.icon
-          }} />
-        <Text style={{ color: "#E5EBEA", alignSelf: "flex-end", fontSize: 16 }} >
-          {!user ? "Coach O" : user.name}
-        </Text>
-      </View>
-      )}
-      style={{ backgroundColor: "#223843" }} />
-    <DrawerContentScrollView {...props}>
-      <DrawerItemList {...props} />
-      <DrawerItem label="Log Out" onPress={() => props.navigation.navigate('Login')} />
-
-    </DrawerContentScrollView>
-  </SafeAreaView>
-  )
-}
+    <SafeAreaView style={{flex: 1}}>    
+      <DrawerItem name="top" label={() => (
+        <View style={{ flex: 1, flexDirection: "row" }}>
+          <Image
+            style={{ height: 70, width: 70, borderRadius: 35, resizeMode: "contain" }}
+            source={{
+              uri: !user ? "http://pngimg.com/uploads/tiger/tiger_PNG23245.png" : user.icon
+            }} />
+          <Text style={{ color: "#E5EBEA", alignSelf: "flex-end", fontSize: 16 }} >
+            {!user ? "Coach O" : user.name}
+          </Text>
+        </View>
+        )}
+        style={{ marginTop: 25 ,backgroundColor: "#223843", paddingTop: 15, paddingBottom: 65}} />
+      <DrawerContentScrollView {...props}>
+        <DrawerItemList {...props} />
+        <DrawerItem label="Log Out" onPress={() => props.navigation.navigate('Login')} />
+  
+      </DrawerContentScrollView>
+    </SafeAreaView>
+    )
+  }
 
 const DrawerNavigator = ({ navigation }) =>  {
   const [user, setUser] = React.useState({})
 
+  let url = '10.0.2.2'
+
   const getProfile = async (id) => {
-    await axios.get(`http://10.0.2.2:8080/user/id/${id}`)
+    await axios.get(`http://${url}:8080/user/id/${id}`)
       .then(post => setUser(post.data))
       .catch(e => console.error(e));
   }
